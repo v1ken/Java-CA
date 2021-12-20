f3b11d4bab17daa538de52ff5b36a9345d240fd9
v1ken <92570365+v1ken@users.noreply.github.com>
2021-12-21 01:09:21 +0800

    Add files via upload

---
+++ src/main/java/com/example/demo/model/LeaveType.java
@@ -0,0 +1,33 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "leavetype")
+@Data
+@NoArgsConstructor
+public class LeaveType {
+	@Id
+	@Column(name = "typeid")
+	private String typeId;
+	@Column(name = "type")
+	private String type;
+	@Column(name = "description")
+	private String description;
+
+	public LeaveType(String typeId, String type, String description) {
+
+		this.typeId = typeId;
+		this.type = type;
+		this.description = description;
+
+	}
+}
